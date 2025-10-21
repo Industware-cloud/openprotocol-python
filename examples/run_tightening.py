aa467b7112f6a6be32e91075d0c8722acb70a090
Tomas Krcka <tomas.krcka@industware.cloud>
2025-10-21 15:43:31 +0200

    examples: extend to accept parameters host, port
    Signed-off-by: Tomas Krcka <tomas.krcka@industware.cloud>

--- examples/run_tightening.py
+++ examples/run_tightening.py
@@ -1,31 +1,27 @@
 import asyncio
-
+import argparse
 from openprotocol.application.client import OpenProtocolClient
 from openprotocol.application.parameter_set import SelectParameterSet
 from openprotocol.application.tightening import LastTighteningResultDataSubscribe
 
 
-async def main():
-    host = "192.168.0.100"
-    port = 5000
-
+async def main(host, port, pset):
     # Create client
     client = OpenProtocolClient.create(host, port)
 
-    # Connect (runs startup handshake automatically)
     try:
         await client.connect()
-        print("Connected to controller.")
+        print(f"Connected to controller at {host}:{port}")
 
-        # Example: send a tightening request
-        pset = SelectParameterSet(3)
-        response = await client.send_receive(pset)
-        print(f"Pset response: {response}")
+        # Send PSET request
+        pset_msg = SelectParameterSet(pset)
+        response = await client.send_receive(pset_msg)
+        print(f"Pset Response: {response}")
 
         # Example: subscribe to asynchronous event
         await client.subscribe(LastTighteningResultDataSubscribe)
         event_msg = await client.get_subscription()
-        print(f"Received event: {event_msg}")
+        print(f"Received Event: {event_msg}")
 
     except Exception as e:
         print(f"Error during communication: {e}")
@@ -36,4 +32,14 @@ async def main():
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(description="OpenProtocol Client")
+
+    parser.add_argument(
+        "--host", type=str, default="192.168.0.100", help="Controller IP address"
+    )
+    parser.add_argument("--port", type=int, default=4545, help="Controller TCP port")
+    parser.add_argument("--pset", type=int, default=3, help="Parameter Set Number")
+
+    args = parser.parse_args()
+
+    asyncio.run(main(args.host, args.port, args.pset))
